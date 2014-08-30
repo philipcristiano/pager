4af65d9dcfd6cf77aa9ec964315d83660f14ea68
Philip Cristiano <philipcristiano@gmail.com>
2014-08-30 19:14:03 -0400

    Cowboy Hello World from tutorial

--- src/pager_app.erl
+++ src/pager_app.erl
@@ -26,7 +26,17 @@ start_ok(App, _Type, {error, Reason}) ->
     erlang:error({app_start_failed, App, Reason}).
 
 start(_StartType, _StartArgs) ->
-    pager_sup:start_link().
+    {ok, _} =pager_sup:start_link(),
+    start_cowboy().
+
+start_cowboy() ->
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", pager_http_handler, []}]}
+    ]),
+    cowboy:start_http(pager_http_listener, 100, [{port, 8080}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
+    pager_http_sup:start_link().
 
 stop(_State) ->
     ok.
